6ff9e16be9be765639f6d04b25c92c7b3b97a178
geneeol <trumpjqk@daum.net>
2025-12-04 23:54:20 +0000

    chore: colorlog lib 추가, util 경로 설정

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,2 +1,4 @@
 from .qa import check_no_error, postprocess_qa_predictions, set_seed
 from .gpu_check import wait_for_gpu_availability
+from .config import get_config
+from .logger import get_logger
